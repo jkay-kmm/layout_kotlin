8b48410c3cde33a3d607208c146d56a2f1835f42
trungdev <nguyenthanhtrungtt@20gmail.com>
2026-03-12 10:23:28 +0700

    listview nâng cao

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -1,30 +1,37 @@
 package com.example.layout
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.layout.databinding.ActivityMainBinding
 private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+   lateinit var adapter: CustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        addEvents()
+        // khai bao danh sach cac item list
+        val list = mutableListOf<OutData>()
+        list.add(OutData(R.drawable.hinh3, "Tiêu đề 1", "mô tả 1"))
+        list.add(OutData(R.drawable.hinh4, "Tiêu đề 2", "mô tả 2"))
+        list.add(OutData(R.drawable.hinh5, "Tiêu đề 3", "mô tả 3"))
+        list.add(OutData(R.drawable.hinh3, "Tiêu đề 1", "mô tả 1"))
+        list.add(OutData(R.drawable.hinh4, "Tiêu đề 2", "mô tả 2"))
+        list.add(OutData(R.drawable.hinh5, "Tiêu đề 3", "mô tả 3"))
+        list.add(OutData(R.drawable.hinh3, "Tiêu đề 1", "mô tả 1"))
+        list.add(OutData(R.drawable.hinh4, "Tiêu đề 2", "mô tả 2"))
+        list.add(OutData(R.drawable.hinh5, "Tiêu đề 3", "mô tả 3"))
+        adapter = CustomAdapter(this, list)
+        // khai bao để lk toi lvPhim
+        val lvPhim = findViewById<ListView>(R.id.lvPhim)
+        lvPhim.adapter = adapter
+        lvPhim.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(this, "Bạn chọn: " + list[position].txt1, Toast.LENGTH_LONG).show()
+        }
     }
-
-    private fun addEvents() {
-        binding.rdHinh1.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.imgHinhAnh.setImageResource(R.drawable.hinh1)
-            }
-        }
-        binding.rdHinh2.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.imgHinhAnh.setImageResource(R.drawable.hinh2)
-            }
-        }
-        binding.btnThoat.setOnClickListener {
-            finish()
-        }
-        }
 
 }
---
+++ app/src/main/java/com/example/layout/OutData.kt
@@ -0,0 +1,7 @@
+package com.example.layout
+
+
+// khai bao contrucstor
+class OutData (val img: Int, val txt1: String, val txt2: String){
+
+}
